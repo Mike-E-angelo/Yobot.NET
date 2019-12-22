6e9886d291554a496b9eb4d2219bb761b92d0da2
Mike-EEE <michael@dragonspark.network>
2019-12-22 05:59:59 -0500

    Initial commit.

---
+++ Yobot/Program.cs
@@ -0,0 +1,34 @@
+using LaunchDarkly.EventSource;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+
+namespace Yobot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var source = new EventSource(new Configuration(new Uri("https://smee.io/3fUXIBJELWjqgj8Q")));
+			source.MessageReceived += (sender, eventArgs) =>
+			                          {
+				                          Debugger.Break();
+			                          };
+
+			source.CommentReceived += (sender, eventArgs) =>
+			                          {
+				                          Debugger.Break();
+			                          };
+
+			source.StartAsync();
+
+			Host.CreateDefaultBuilder(args)
+			    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+			    .Build()
+			    .Run();
+
+			source.Close();
+		}
+	}
+}
